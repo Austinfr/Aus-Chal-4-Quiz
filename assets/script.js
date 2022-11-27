1dc6bf1cf756cc923f4d0439d9852fd4315c87a0
Austie <wustinah20@gmail.com>
2022-11-27 02:38:45 -0800

    Added commented questions in script.js to implement later. Added some style and classes to css and some elements on the webpage for index so there's something to see

---
+++ assets/script.js
@@ -0,0 +1,32 @@
+/*  Put Questions here
+    Which html tag does javascript go inside?
+    1.<javascript>
+    2.<script> //correct
+    3.<js>
+    4.<scripting>
+
+    What function is used to display a message popup?
+    1.alert() //correct
+    2.Math.random()
+    3.popup()
+    4.message()
+
+    How do you create a function in javascript?
+    1.function = myFunction()
+    2.function:myFunction()
+    3.function myFunction() //correct
+    4.function()
+
+    Which is the correct way to write a javascript array?
+    1.var trees = 1 = ("oak"),2 = ("birch"),3 = ("aspin")
+    2.var trees = "oak","birch","aspin"
+    3.var trees = (1:"oak",2:"birch",3:"aspin")
+    4.var trees = ["oak", "birch", "aspin"] //correct
+
+    How do you compare type and variable equality?
+    1.=
+    2.=== //correct
+    3.==
+    4.====
+
+*/
