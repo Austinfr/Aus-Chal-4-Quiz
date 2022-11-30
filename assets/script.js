1dd27cab91df38e77d3a45b9ccd99a3025526305
Austie <wustinah20@gmail.com>
2022-11-29 23:27:49 -0800

    added more to webpage, a hidden end screen part and some more style to css classes

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,5 @@
-/*  Put Questions here
+/*  Put Questions here:
+
     Which html tag does javascript go inside?
     1.<javascript>
     2.<script> //correct
@@ -23,10 +24,40 @@
     3.var trees = (1:"oak",2:"birch",3:"aspin")
     4.var trees = ["oak", "birch", "aspin"] //correct
 
-    How do you compare type and variable equality?
+    How do you compare type and variable equality in javascript?
     1.=
     2.=== //correct
     3.==
     4.====
 
+    How do you declare a varaible in javascript?
+    1.var i = 50 //correct
+    2.int i = 50
+    3.variable 1 = 50
+    4.i = 50
+
 */
+
+//variables
+var highScoreButton = document.querySelector(".highScoresNTime > a");
+var sButton = document.querySelector("#startButton");
+console.log(highScoreButton);
+
+//functions
+function clearPage(){
+    document.body.innerHTML = '';
+}
+
+function beginQuiz(event){
+    event.preventDefault();
+    document.querySelector(".start").innerHTML = '';
+    console.log("cleared");
+}
+
+function gotoHighscores(){
+    clearPage();
+}
+
+//main logic
+sButton.addEventListener("click", beginQuiz);
+highScoreButton.addEventListener("click", gotoHighscores);
